db831fa43b1948c9e44239e861ea2a58ee68937e
mwarr5 <madelyn.warr@gmail.com>
2025-06-06 13:19:42 -0600

    Modified find_anchors to run faster and finished run_all_chaining

--- find_anchors.py
+++ find_anchors.py
@@ -7,6 +7,7 @@ import numpy as np
 # Should be tsv of motif1, motif2, start1, start2, score. Motif1 and motif2 are sorted in alphabetical order
 # Returns a dict that pairs (motif1, motif2) : (start1, start2, score)
 def read_motif_pairs(min_score):
+    print("Reading Scores File")
     SCORES_FILE_NAME = "scores.tsv"
 
     pairwise_score_dict = {}
@@ -15,7 +16,6 @@ def read_motif_pairs(min_score):
         next(pair_scores)
         for line in pair_scores :
             line_arr = line.rstrip().split('\t')
-            print(line_arr)
             if float(line_arr[4]) > min_score:
                 pairwise_score_dict[(line_arr[0], line_arr[1])] = (int(line_arr[2]), int(line_arr[3]), float(line_arr[4]))
     return pairwise_score_dict
@@ -47,18 +47,20 @@ def read_motif_pairs(min_score):
 
 #     return anchors
 
+
 # Finds pairwise for all chromosomes            
 # Make sure to clear the output folder first since we are appending to files 
-# Printed output is a file for each chr chr pair, each line is genome1, location, genome2, location, score         
+# Printed output is a file for each chr chr pair, each line has genome\tloc\tgenome\tloc\t...genome\tloc##genome\tloc\t....genome\tloc\nscore\n       
 def find_anchors_all_pairs(data_dir, out_dir, score_dict) :
-    # Holds where each motif is located {MOTIF: [(chr, genome, loc)]}
-    motif_loc_dict = defaultdict(list)
+    # Holds where each motif is located {MOTIF: {chr : [(genome, loc), (genome, loc)]), chr2 : [...]}, MOTIF2 ...}
+    motif_loc_dict = defaultdict(lambda: defaultdict(list))
 
+    print("Filling Motif Dict")
 
     for file_path in Path(data_dir).rglob("*"):
         if file_path.is_file() and file_path.suffix == ".tsv":
-
             # Save folder name and file name to give to the dict
+            # Folder name is the acr (chr), file name is the genome
             folder_name = file_path.parent.name
             file_name = file_path.stem
 
@@ -67,56 +69,101 @@ def find_anchors_all_pairs(data_dir, out_dir, score_dict) :
                 # Ignore first line
                 next(f)
                 for line in f:
+                    # Each line is motif, location
                     line_arr = line.rstrip().split('\t')
-                    motif_loc_dict[line_arr[0]].append((folder_name, file_name, int(line_arr[1])))
+                    motif_loc_dict[line_arr[0]][folder_name].append((file_name, int(line_arr[1])))
     
-    # print(motif_loc_dict)
+
+    print("Finding Anchors")
+    print(f"Number of Pairs Total: {len(score_dict.keys())}")
+    progress_counter = 0
 
     # Faster to iterate through the keys of the match dict
     for pair in score_dict.keys() :
+        if progress_counter % 10000 == 0 :
+            if progress_counter != 0 :
+                print(f"Done: {progress_counter}")
+        progress_counter += 1
+        
+        ## REMINDER: {MOTIF: {chr : [(genome, loc), (genome, loc)]), chr2 : [...]}, MOTIF2 ...}
         # If pair is the same, iterate through combinations
         # We don't need to add start points
         if pair[0] == pair[1] :
-            for location_1, location_2 in itertools.combinations(motif_loc_dict[pair[0]], 2):
-                if location_1[0] != location_2[0] :
+            # Iterate through all pairs of acrs
+            for acr1, acr2 in itertools.combinations(motif_loc_dict[pair[0]].keys(), 2):
+                
+                # No need to worry about same acrs because combinations
+                sorted_pair = sorted([acr1, acr2])
+                out_file = Path(out_dir) / (sorted_pair[0] + '_' + sorted_pair[1] + '.tsv')
+                
+                # Don't need to worry about offsets
+                with open(out_file, "a") as f:
+                    # Write all genome loc pairs 
+                    for i, genome_and_loc in enumerate(motif_loc_dict[pair[0]][sorted_pair[0]]) :
+                        # This guarantees the tabs are correct
+                        if i == 0 :
+                            f.write(f"{genome_and_loc[0]}\t{genome_and_loc[1]}")
+                        else :
+                            f.write(f"\t{genome_and_loc[0]}\t{genome_and_loc[1]}")
+                    # separator
+                    f.write("##")
+                    for i, genome_and_loc in enumerate(motif_loc_dict[pair[1]][sorted_pair[1]]) :
+                        if i == 0 :
+                            f.write(f"{genome_and_loc[0]}\t{genome_and_loc[1]}")
+                        else :
+                            f.write(f"\t{genome_and_loc[0]}\t{genome_and_loc[1]}")
+                    f.write(f"\n{score_dict[pair][2]}\n")
 
-                    # We don't need to worry about offsets since it should all be 1
-                    sorted_pair = sorted([location_1, location_2])
-                    out_file = Path(out_dir) / (sorted_pair[0][0] + '_' + sorted_pair[1][0] + '.tsv')
+        # Case where they are different
+        else :
+            for acr1 in motif_loc_dict[pair[0]].keys() :
+                for acr2 in motif_loc_dict[pair[1]].keys() :
+                    if acr1 != acr2 :
+                        # Sort this, we then need to add the starts to the correct one (so get the argsort)
+                        sorted_order = np.argsort([acr1, acr2])
+                        sorted_pair = sorted([acr1, acr2])
+                        out_file = Path(out_dir) / (sorted_pair[0] + '_' + sorted_pair[1] + '.tsv')
+
+                        
+                        # Worry about offsets
+                        with open(out_file, "a") as f:
+                            # Write all genome loc pairs 
+                            for i, genome_and_loc in enumerate(motif_loc_dict[pair[sorted_order[0]]][sorted_pair[0]]) :
+                                # This guarantees the tabs are correct
+                                if i == 0 :
+                                    f.write(f"{genome_and_loc[0]}\t{genome_and_loc[1] + score_dict[pair][sorted_order[0]]}")
+
+                                else :
+                                    f.write(f"\t{genome_and_loc[0]}\t{genome_and_loc[1] + score_dict[pair][sorted_order[0]]}")
+
+                            # separator
+                            f.write("##")
+                            for i, genome_and_loc in enumerate(motif_loc_dict[pair[sorted_order[1]]][sorted_pair[1]]) :
+
+                                if i == 0 :
+                                    f.write(f"{genome_and_loc[0]}\t{genome_and_loc[1] + score_dict[pair][sorted_order[1]]}")
+                                else :
+                                    f.write(f"\t{genome_and_loc[0]}\t{genome_and_loc[1] + score_dict[pair][sorted_order[1]]}")
+                            f.write(f"\n{score_dict[pair][2]}\n")
                     
-                    with open(out_file, "a") as f:
-                        f.write(f"{sorted_pair[0][1]}\t{sorted_pair[0][2]}\t{sorted_pair[1][1]}\t{sorted_pair[1][2]}\t{score_dict[pair][2]}\n")
-
         # If pair different
         # We need to add start points
-        else :
-            for location_1 in motif_loc_dict[pair[0]] :
-                for location_2 in motif_loc_dict[pair[1]] :
-                    if location_1[0] != location_2[0] :
-                        # Sort this, we then need to add the starts to the correct one (so get the argsort)
-                        sorted_order = np.argsort([location_1[0], location_2[0]])
-                        sorted_pair = sorted([location_1, location_2])
-                        out_file = Path(out_dir) / (sorted_pair[0][0] + '_' + sorted_pair[1][0] + '.tsv')
+        # else :
+        #     for location_1 in motif_loc_dict[pair[0]] :
+        #         for location_2 in motif_loc_dict[pair[1]] :
+        #             if location_1[0] != location_2[0] :
+        #                 # Sort this, we then need to add the starts to the correct one (so get the argsort)
+        #                 sorted_order = np.argsort([location_1[0], location_2[0]])
+        #                 sorted_pair = sorted([location_1, location_2])
+        #                 out_file = Path(out_dir) / (sorted_pair[0][0] + '_' + sorted_pair[1][0] + '.tsv')
                         
-                        with open(out_file, "a") as f:
-                            f.write(f"{sorted_pair[0][1]}\t{sorted_pair[0][2] + score_dict[pair][sorted_order[0]]}\t{sorted_pair[1][1]}\t{sorted_pair[1][2]  + score_dict[pair][sorted_order[1]]}\t{score_dict[pair][2]}\n")
-
-
-DATA_DIR = '../test_folder'
-OUT_DIR = '../test_out'
-MIN_SCORE = 0.6
-
-score_dict = read_motif_pairs(MIN_SCORE)
-print(score_dict)
-exit(0)
-# find_anchors_all_pairs(DATA_DIR, OUT_DIR, score_dict)
-    
+        #                 with open(out_file, "a") as f:
+        #                     f.write(f"{sorted_pair[0][1]}\t{sorted_pair[0][2] + score_dict[pair][sorted_order[0]]}\t{sorted_pair[1][1]}\t{sorted_pair[1][2]  + score_dict[pair][sorted_order[1]]}\t{score_dict[pair][2]}\n")
         
 
-
-
-
-
+DATA_DIR = '../Preprocessed_Data'
+OUT_DIR = '../Anchors_min1'
+MIN_SCORE = 1.0
 
 
 
@@ -126,3 +173,7 @@ exit(0)
 #               ("AAAA", "ACCG") : (3, 0, 2),
 #                 ("AAAAACC", "TAACC") : (3, 1, 5) 
 #               }
+
+
+score_dict = read_motif_pairs(MIN_SCORE)
+find_anchors_all_pairs(DATA_DIR, OUT_DIR, score_dict)
--- run_all_chaining.py
+++ run_all_chaining.py
@@ -1,37 +1,67 @@
 from pathlib import Path
 from collections import defaultdict
 import numpy as np
+import random
+from chaining import chain
+import os
+import time
 
-def run_chaining_all(root_dir):
+def run_chaining_all(input_root_dir, output_dir):
 
-    genome_pairs = defaultdict(list)
-
-    search_dir = Path(root_dir)
+    search_dir = Path(input_root_dir)
     #loop through all files
     for acr_combo in search_dir.glob('*'):
+        genome_pairs = defaultdict(list)
         with open(acr_combo) as acr_combo_file:
             for line in acr_combo_file:
-                line_arr = line.split("\t")
-                #get the pair and sort it
-                pair = [line_arr[0], line_arr[2]]
-                pair_sorted = tuple(sorted(pair))
+                line_arr = line.split("##")
+                motif_match_1 = line_arr[0].strip().split("\t")
+                motif_match_2 = line_arr[1].strip().split("\t")
 
-                pair_indices = (line_arr[1], line_arr[3].strip())
-                #see if the indices need to be swapped to match up with how the 
-                #pair was sorted
-                pair_argsort = np.argsort(pair)
-                pair_order = (line_arr[1], line_arr[2])
-                
-                if pair_order[0] == 1:
-                    #swap the order
-                    temp = pair_indices[0]
-                    pair_indices[0] = pair_indices[1]
-                    pair_indices[1] = temp 
-                    
-                
-                genome_pairs[pair_sorted].append(pair_indices)
-    print(genome_pairs)
+                try:
+                    assert(len(motif_match_1) % 2 == 0 and len(motif_match_2) % 2 == 0)
+                except:
+                    print(f"ERROR! There are not the same number of genomes as locations in an entry in {str(acr_combo)}")
+                for i in range(0, len(motif_match_1), 2):
+                    for j in range(0, len(motif_match_2), 2):
+                        #get the pair and sort it
+                        genome_pair = [motif_match_1[i], motif_match_2[j]]
+                        genome_pair_sorted = tuple(sorted(genome_pair))
 
-#with open("chr1_chr2.txt", "w") as file:
-    #file.write("abc\t5\tdef\t6\nabc\t4\thij\t3\nklm\t3\tabc\t7\nhij\t2\tdef\t7")
-run_chaining_all("test_folder")
+                        pair_indices = (int(motif_match_1[i+1]), int(motif_match_2[j+1].strip()))
+                        #see if the indices need to be swapped to match up with how the 
+                        #pair was sorted
+                        pair_argsort = np.argsort(genome_pair)
+                        if pair_argsort[0] == 1:
+                            #swap the order
+                            pair_indices = (pair_indices[1], pair_indices[0])
+                            
+                        genome_pairs[genome_pair_sorted].append(pair_indices)
+        with open(f"{output_dir}/chr3_chr_4_output.tsv", "w") as output_file:
+            for pair in genome_pairs.keys():
+                chain_len = chain(genome_pairs[pair])
+                output_file.write(f"{pair[0]}\t{pair[1]}\t{chain_len}\n")
+
+
+
+
+def create_test_file(num_lines, num_per_line):
+    with open("test_folder/chr3_chr4.txt", "w") as file: 
+        for k in range(num_lines):
+            for i in range(num_per_line):
+                rand_str = ""
+                for j in range(5):
+                    rand_str += chr(random.randint(97, 122))
+                file.write(f"{rand_str}\t{random.randint(1, 100)}\t")
+            file.write("##")
+            for i in range(num_per_line):
+                rand_str = ""
+                for j in range(5):
+                    rand_str += chr(random.randint(97, 122))
+                file.write(f"{rand_str}\t{random.randint(1, 100)}\t")
+            file.write("\n")
+
+create_test_file(4, 79000)
+start_time = time.time()
+run_chaining_all("test_folder", "test_output_folder")
+print(f"Time: {time.time() - start_time}")
