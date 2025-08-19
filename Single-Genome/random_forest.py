372bc749d2d6827790a0a7823598359d8a038abe
mwarr5 <madelyn.warr@gmail.com>
2025-08-19 12:05:27 -0600

    Added random baseline

--- Single-Genome/random_forest.py
+++ Single-Genome/random_forest.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestClassifier #type: ignore
+from sklearn.tree import DecisionTreeClassifier #type: ignore
 import random
 from collections import defaultdict
 
@@ -86,7 +86,7 @@ from chaining with the representative regions
 Outputs a tuple. The first value is a list with the predicted classes and the second value is a list
 of the actual classes. 0 indicates a random region and 1 indicates an ACR.
 '''          
-def train_predict(train_dict, test_dict, settings={}):
+def train_predict(train_dict, test_dict, fraction=.5, settings={}):
     feature_train = []
     labels_train = []
     for region, feature_list in train_dict.items():
@@ -98,18 +98,30 @@ def train_predict(train_dict, test_dict, settings={}):
     
     feature_test = []
     labels_test = []
+    threshold = fraction * len(test_dict)
+    acr_count = 0
+    to_delete = []
     
     for region, feature_list in test_dict.items():
-        feature_test.append(feature_list)
         if "rand" in region:
             labels_test.append(0)
+            feature_test.append(feature_list)
         else:
-            labels_test.append(1)
+            if acr_count > threshold: #we have included enough ACRs
+                to_delete.append(region) #save for deletion
+            else:
+                labels_test.append(1)
+                feature_test.append(feature_list)
+                acr_count += 1
 
-    classifier = RandomForestClassifier(**settings)
+    classifier = DecisionTreeClassifier(**settings)
     classifier.fit(feature_train, labels_train)
     return classifier.predict(feature_test), labels_test
 
+'''
+Calculates and returns a dictionary of true and false positives,
+as well as true and false negatives.
+'''
 def compare_predict_and_actual(predict_list, actual_list):
     true_pos = 0
     false_pos = 0
@@ -128,18 +140,49 @@ def compare_predict_and_actual(predict_list, actual_list):
                 true_neg += 1
     return {"true_pos": true_pos, "false_pos": false_pos, "true_neg": true_neg, "false_neg": false_neg}
 
-def driver(score_file, output_file):
-    base = "/home/mwarr/Data/One_Genome/exp3_randomforest"
-    rand_acr_file = f"{base}/setb_and_rand_list.txt"
-    rep_list_file = f"{base}/seta_rep_hier_loc_list.txt"
-    score_file = f"{base}/{score_file}"
+'''
+<score_file> should be a file with the train and set regions chained with the representative
+regions. A pairwise chaining file should not be passed in here.
+<rep_list_file> should be a file with the names of all the reference ACRs, each on their own line.
+
+This function randomly creates a test and train set, trains a model based on the train set feature
+vectors consisting of chaining scores to some central ACRs, predicts the region type of the regions in
+test_set, and then returns a dictionary of performance statistics, as well as a dictionary of control statistics
+(random predictions)
+'''
+def process(score_file, rep_list_file, fraction=.5):
+    rand_acr_file = "/home/mwarr/Data/One_Genome/exp3_randomforest/setb_and_rand_list.txt"
     train_set, test_set = get_region_sets(rand_acr_file)
     rep_lst, index_dict = rep_list(rep_list_file)
     train_dict, test_dict = get_chain_dicts(score_file, train_set, test_set, rep_lst, index_dict)
-    predict, actual = train_predict(train_dict, test_dict)
-    print(compare_predict_and_actual(predict, actual))
+    predict, actual = train_predict(train_dict, test_dict, fraction=fraction)
+    predict_control = [random.randint(0, 1) for _ in range(len(actual))]
+    return compare_predict_and_actual(predict, actual), compare_predict_and_actual(predict_control, actual)
+
+'''
+Driver for experiment where <fraction> of TEST regions are ACRs and <1-fraction> are not. Also runs a control by randomly predicting regions.
+Outputs results to <output_file> in the format:
+<type>
+actual <accuracy>
+control <accuracy
+'''
+def driver(output_file, fraction=.5):
+    base = "/home/mwarr/Data/One_Genome/exp3_randomforest"
+    score_files = ["Chaining_local_rep_weight.tsv", "Chaining_global_rep_weight.tsv", "Chaining_global_consensus.tsv", "Chaining_local_consensus.tsv"]
+    with open(output_file, "w") as file:
+        for score_file in score_files:
+            if "rep" in score_file:
+                stat_dict, stat_dict_control = process(f"{base}/{score_file}", f"{base}/seta_rep_clustered_loc_list.txt", fraction=fraction)
+            else:
+                stat_dict, stat_dict_control = process(f"{base}/{score_file}", f"{base}/seta_consensus_DAPv1_clustered_loc_list.txt", fraction=fraction)
+            
+            accuracy = (stat_dict["true_pos"] + stat_dict["true_neg"]) / (stat_dict["true_pos"] + stat_dict["true_neg"] + stat_dict["false_pos"] + stat_dict["false_neg"])
+            accuracy_ctrl = (stat_dict_control["true_pos"] + stat_dict_control["true_neg"]) / (stat_dict_control["true_pos"] + stat_dict_control["true_neg"] + stat_dict_control["false_pos"] + stat_dict_control["false_neg"])
+            file.write(f"{score_file[:-4]}\n")
+            file.write(f"actual {round(accuracy, 2)}\n")
+            file.write(str(stat_dict) + "\n")
+            file.write(f"control {round(accuracy_ctrl, 2)}\n")
+            file.write(str(stat_dict_control) + "\n")
 
 if __name__ == "__main__":
-    score_files = ["Chaining_local_rep_weight.tsv", "Chaining_global_rep_weight.tsv", "Chaining_global_consensus.tsv", "Chaining_local_consensus.tsv"]
-    for score_file in score_files:
-        driver(score_file, "/home/mwarr/Data/One_Genome/exp3_randomforest" + score_file[9:-4] + ".txt")
+    driver("/home/mwarr/Data/One_Genome/exp3_randomforest/results/90-10_decision_tree.txt", fraction=.1)
